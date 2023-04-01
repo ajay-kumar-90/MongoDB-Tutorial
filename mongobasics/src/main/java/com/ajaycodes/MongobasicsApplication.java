afb75f32a72e3765f5c0e03c19a0520f98727585
Ajay <ajay_kumar_1990@live.com>
2023-04-02 05:07:25 +0530

    Initial commit

---
+++ mongobasics/src/main/java/com/ajaycodes/MongobasicsApplication.java
@@ -0,0 +1,13 @@
+package com.ajaycodes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongobasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongobasicsApplication.class, args);
+	}
+
+}
